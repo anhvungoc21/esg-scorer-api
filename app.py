b923249c97f7c123bff17a949095137e4239ee6b
Anh Vu <anhvungoc.21@gmail.com>
2022-09-25 12:13:37 -0500

    fixing heroku

--- app.py
+++ app.py
@@ -45,5 +45,6 @@ def get_esg_visual():
     res = formatTextESG(text)
     return res
 
-
-
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
